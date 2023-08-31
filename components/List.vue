4d7283dd5e28edafb7b3feb05b39105e8d0acd88
henkvhest <henkvhest@hotmail.com>
2023-08-31 16:43:00 +0200

    2 files modified, 1 added

---
+++ components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="list">
+    <slot></slot>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<style lang="scss" scoped>
+.list {
+  margin: 0;
+  padding: 0;
+
+  > li + li {
+    margin-top: 16px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 definePageMeta({});
+import { List } from '../components/List.vue';
 </script>
 <template>
   <div>Nuxt 3 Playground</div>
+  <List
+    ><li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </List>
 </template>
